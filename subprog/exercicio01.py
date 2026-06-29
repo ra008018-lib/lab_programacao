0b3dd639eb31f358ab90951883b6fca73f099a49
Pedro Lucas Narciso Soares <008018@libertas.edu.br>
2026-06-29 14:54:59 -0300

    Add files via upload

---
+++ subprog/exercicio01.py
@@ -0,0 +1,17 @@
+# Nota acima de 6 = "Aprovado"
+# Nota entre 4 e 6 = "Verificação Suplementar"
+# Nota abaixo de 4 = "Reprovado"
+
+def menu():
+    print('---------------------')
+    print("Em qual média está esse aluno: ")
+    print('1 - Acima de 6')
+    print('2 - Entre 4 e 6')
+    print('3 - Abaixo de 4')
+    print('---------------------')
+
+menu()
+opcao = int(input('Digite a opção desejada: '))
+
+menu()
+menu()
